2711b6eba8264a66ab52b3864c97b37b9beb9669
Quy Tran Xuan <quy.tranxuan@nashtechglobal.com>
2024-08-26 21:55:58 +0700

    update resources

--- aws/main.tf
+++ aws/main.tf
@@ -2,33 +2,20 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.11.0"
+      version = "5.63.0"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "2.10.1"
+      version = "2.15.0"
     }
   }
 }
 
 provider "aws" {
-  region  = "us-east-1"
-  profile = "qtran"
+  region  = "ap-southeast-1"
   default_tags {
     tags = {
-      Project = "Pratical DevOps for Dev"
-      Org     = "HNVN"
-    }
-  }
-}
-provider "helm" {
-  kubernetes {
-    host = aws_eks_cluster.this.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.this.certificate_authority.0.data)
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      args = ["eks", "get-token", "--cluster-name", aws_eks_cluster.this.name, "--profile", "qtran"]
-      command = "aws"
+      Course = "Pratical DevOps"
     }
   }
 }
@@ -79,95 +66,54 @@ resource "aws_route_table_association" "public_internet" {
   route_table_id = aws_route_table.this.id
 }
 
-resource "aws_network_acl" "internet_acl" {
-  vpc_id     = aws_vpc.msavnet.id
-  subnet_ids = [for subnet in aws_subnet.public: subnet.id]
+# resource "aws_network_acl" "internet_acl" {
+#   vpc_id     = aws_vpc.msavnet.id
+#   subnet_ids = [for subnet in aws_subnet.public: subnet.id]
 
-  dynamic "ingress" {
-    for_each = var.public_acl_ingress
-    content {
-      from_port  = ingress.value["from_port"]
-      to_port    = ingress.value["to_port"]
-      rule_no    = ingress.value["rule_no"]
-      action     = ingress.value["action"]
-      protocol   = ingress.value["protocol"]
-      cidr_block = ingress.value["cidr_block"]
-    }
-  }
+#   dynamic "ingress" {
+#     for_each = var.public_acl_ingress
+#     content {
+#       from_port  = ingress.value["from_port"]
+#       to_port    = ingress.value["to_port"]
+#       rule_no    = ingress.value["rule_no"]
+#       action     = ingress.value["action"]
+#       protocol   = ingress.value["protocol"]
+#       cidr_block = ingress.value["cidr_block"]
+#     }
+#   }
 
-  egress {
-    from_port  = 0
-    to_port    = 0
-    rule_no    = 100
-    action     = "allow"
-    protocol   = -1
-    cidr_block = "0.0.0.0/0"
-  }
+#   egress {
+#     from_port  = 0
+#     to_port    = 0
+#     rule_no    = 100
+#     action     = "allow"
+#     protocol   = -1
+#     cidr_block = "0.0.0.0/0"
+#   }
+# }
+
+resource "aws_ecr_repository" "backend" {
+  name = "ntg-garage-backend"
 }
-
-# EC2 Instance
-resource "aws_security_group" "ec2sg" {
-  name        = "Agentsg"
-  description = "CI/CD agent admin access"
-  vpc_id      = aws_vpc.msavnet.id
-
-  dynamic "ingress" {
-    for_each = var.sg_ingress
-    content {
-      from_port   = ingress.value["from_port"]
-      to_port     = ingress.value["to_port"]
-      protocol    = ingress.value["protocol"]
-      cidr_blocks = [ingress.value["cidr_block"]]
-    }
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = -1
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-resource "aws_key_pair" "ec2_ssh" {
-  key_name   = var.ssh_key_name
-  public_key = var.public_key
-}
-resource "aws_instance" "jenkins" {
-  ami                    = var.ami
-  instance_type          = "t2.micro"
-  key_name               = aws_key_pair.ec2_ssh.key_name
-  subnet_id              = aws_subnet.public[0].id
-  vpc_security_group_ids = [aws_security_group.ec2sg.id]
-  root_block_device {
-    volume_type = "gp3"
-    volume_size = var.root_volume_size
-  }
-
-  tags = {
-    "Name" = "Jenkins controller"
-  }
-}
-
-resource "aws_ecr_repository" "msabackend" {
-  name = "massamplebackend"
-}
-resource "aws_ecr_repository" "msafrontend" {
-  name = "massamplefrontend"
+resource "aws_ecr_repository" "frontend" {
+  name = "ntg-garage-frontend"
 }
 
 # EKS cluster
 # Policy AmazonEKSClusterPolicy, AmazonEKSVPCResourceController
-data "aws_iam_role" "eksclusterrole" {
+data "aws_iam_role" "eks_service_role" {
   name = "AmazonEKSClusterRole"
 }
+
 resource "aws_eks_cluster" "this" {
-  name     = var.eks_cluster_name
-  role_arn = data.aws_iam_role.eksclusterrole.arn
+  name = var.eks_cluster_name
+  role_arn = data.aws_iam_role.eks_service_role.arn
   vpc_config {
-    subnet_ids          = [for subnet in aws_subnet.public : subnet.id]
+    subnet_ids = aws_subnet.public.*.id 
     public_access_cidrs = var.eks_public_access_cidrs
   }
 }
+
 resource "aws_eks_addon" "this" {
   count = length(local.eks_add_ons)
 
@@ -176,62 +122,62 @@ resource "aws_eks_addon" "this" {
 }
 
 # Policy AmazonEC2ContainerRegistryReadOnly, AmazonEKS_CNI_Policy, AmazonEKSWorkerNodePolicy
-data "aws_iam_role" "eksnoderole" {
-  name = "AWSEKSNodeGroup"
+data "aws_iam_role" "eks_worker_role" {
+  name = "AmazonEKSWorkerRole"
 }
 resource "aws_eks_node_group" "this" {
   cluster_name  = aws_eks_cluster.this.name
-  node_group_name = "eks-practcaldevops-nodegroup"
-  node_role_arn = data.aws_iam_role.eksnoderole.arn
+  node_group_name = "practicaldevops-nodegroup"
+  node_role_arn = data.aws_iam_role.eks_worker_role.arn
   scaling_config {
     desired_size = 1
-    max_size     = 1
+    max_size     = 2
     min_size     = 0
   }
-  subnet_ids     = [for subnet in aws_subnet.public : subnet.id]
-  ami_type       = "AL2_x86_64"
+  subnet_ids     = aws_subnet.public.*.id
+  ami_type       = "AL2023_x86_64_STANDARD"
   disk_size      = 8
   capacity_type  = "SPOT"
-  instance_types = ["t2.medium"]
+  instance_types = ["t3.medium"]
 }
 
-resource "helm_release" "prometheus" {
-  name = "prometheus"
-  chart = "prometheus"
-  repository = "https://prometheus-community.github.io/helm-charts"
-  namespace = "prometheus"
-  create_namespace = true
-  cleanup_on_fail = true
+# resource "helm_release" "prometheus" {
+#   name = "prometheus"
+#   chart = "prometheus"
+#   repository = "https://prometheus-community.github.io/helm-charts"
+#   namespace = "prometheus"
+#   create_namespace = true
+#   cleanup_on_fail = true
 
-  dynamic "set" {
-    for_each = var.prometheus_chart_values
+#   dynamic "set" {
+#     for_each = var.prometheus_chart_values
     
-    iterator = chart_value
-    content {
-      name = chart_value.value["name"]
-      value = chart_value.value["value"]
-    }
-  }
+#     iterator = chart_value
+#     content {
+#       name = chart_value.value["name"]
+#       value = chart_value.value["value"]
+#     }
+#   }
 
-  depends_on = [ aws_eks_node_group.this ]
-}
+#   depends_on = [ aws_eks_node_group.this ]
+# }
 
-resource "helm_release" "grafana" {
-  name = "grafana"
-  chart = "grafana"
-  repository = "https://grafana.github.io/helm-charts"
-  namespace = "grafana"
-  create_namespace = true
-  cleanup_on_fail = true
+# resource "helm_release" "grafana" {
+#   name = "grafana"
+#   chart = "grafana"
+#   repository = "https://grafana.github.io/helm-charts"
+#   namespace = "grafana"
+#   create_namespace = true
+#   cleanup_on_fail = true
 
-  dynamic "set" {
-    for_each = var.grafana_chart_values
-    iterator = chart_value
-    content {
-      name = chart_value.value["name"]
-      value = chart_value.value["value"]
-    }
-  }
+#   dynamic "set" {
+#     for_each = var.grafana_chart_values
+#     iterator = chart_value
+#     content {
+#       name = chart_value.value["name"]
+#       value = chart_value.value["value"]
+#     }
+#   }
 
-  depends_on = [ aws_eks_node_group.this ]
-}
+#   depends_on = [ aws_eks_node_group.this ]
+# }
--- aws/variables.tf
+++ aws/variables.tf
@@ -39,15 +39,15 @@ variable "private_subnet_tags" {
   description = "(Optional) A map of tags to assign to the resource."
 }
 
-variable "ssh_key_name" {
-  type        = string
-  description = "(Optional) The name for the key pair. If neither key_name nor key_name_prefix is provided, Terraform will create a unique key name using the prefix terraform-."
-}
+# variable "ssh_key_name" {
+#   type        = string
+#   description = "(Optional) The name for the key pair. If neither key_name nor key_name_prefix is provided, Terraform will create a unique key name using the prefix terraform-."
+# }
 
-variable "public_key" {
-  type        = string
-  description = "(Required) The public key material"
-}
+# variable "public_key" {
+#   type        = string
+#   description = "(Required) The public key material"
+# }
 
 variable "public_acl_ingress" {
   type        = list(map(string))
@@ -55,22 +55,22 @@ variable "public_acl_ingress" {
   description = "(Optional) Specifies an ingress rule."
 }
 
-variable "root_volume_size" {
-  type        = number
-  default     = null
-  description = "(Optional) Size of the volume in gibibytes (GiB)."
-}
+# variable "root_volume_size" {
+#   type        = number
+#   default     = null
+#   description = "(Optional) Size of the volume in gibibytes (GiB)."
+# }
 
-variable "ami" {
-  type        = string
-  description = "AMI to use for the instance."
-}
+# variable "ami" {
+#   type        = string
+#   description = "AMI to use for the instance."
+# }
 
-variable "sg_ingress" {
-  type        = list(map(string))
-  default     = []
-  description = "(Optional) Specifies an ingress rule."
-}
+# variable "sg_ingress" {
+#   type        = list(map(string))
+#   default     = []
+#   description = "(Optional) Specifies an ingress rule."
+# }
 
 variable "eks_cluster_name" {
   type        = string
